0aaf73d897abead4dd96ade1dc555be03e3af67b
kareena tomiah <kareena1508@gmail.com>
2025-06-02 12:07:35 +0400

    message

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
+# This controller handles the ask and answer logic for the coaching app
 class QuestionsController < ApplicationController
   def ask
+    ''
   end
 
   def answer
+    @question = params[:question]
+
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question&.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
